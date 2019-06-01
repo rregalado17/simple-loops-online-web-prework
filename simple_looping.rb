a6c5a9d4ea2c7dde87207dacb7fdc96f5dbbec53
magical-flow-3186 <rregalado17@gmail.com>
2019-06-01 22:22:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,7 @@ def loop_iterator(number_of_times)
 number_of_times = 0 
 loop do 
   puts "Welcome to Flatiron School's Web Development Course!"
-   if number_of_times 
+   if number_of_times == 7
     break
   end
   end
