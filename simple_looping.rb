c6fef6d31b14e9b973af7367c16c3cab6e1d5e6c
magical-flow-3186 <rregalado17@gmail.com>
2019-06-01 22:06:35 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,6 +13,7 @@ loop do
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  if counter > 
 end
 end
 
