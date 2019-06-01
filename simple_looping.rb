b09b047cb5f11593444d20722b91610619b23303
magical-flow-3186 <rregalado17@gmail.com>
2019-06-01 22:06:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,6 +2,7 @@
 
 def loop_iterator(number_of_times)
 loop do 
+  counter += 
 
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
